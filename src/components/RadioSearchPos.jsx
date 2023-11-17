cf61a7f40ccd871d25b0b57c596bab4d0099a6cd
Eder Dominguez <ederdominguez@Mac-mini-de-Eder.local>
2023-11-17 14:00:24 -0600

    add filter radio

---
+++ src/components/RadioSearchPos.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export const RadioSearchPos = ({value, selectRadio}) => {
+
+
+    const handleOptionChange = (e) => {
+        selectRadio(e.target.value);
+      };
+
+  return (
+    <>
+
+<ul className="items-center text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex ">
+    <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r ">
+        <div className="flex items-center px-3">
+            <input 
+                id="horizontal-list-radio-license" 
+                type="radio" value="todos" 
+                name="list-radio" 
+                className="w-4 h-4 text-sqgreen-900 bg-gray-100 border-gray-300 focus:ring-sqgreen-900 accent-sqgreen-900  " 
+                checked={value === "todos"}
+                onChange={handleOptionChange}
+            />
+            <label for="horizontal-list-radio-license" className="w-full py-3 px-2 ms-2 text-sm font-medium text-gray-900 ">Todos </label>
+        </div>
+    </li>
+    <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r ">
+        <div className="flex items-center px-3">
+            <input
+                id="horizontal-list-radio-license" 
+                type="radio" value="conectados" 
+                name="list-radio" 
+                className="w-4 h-4 text-sqgreen-900 bg-gray-100 border-gray-300 focus:ring-sqgreen-900 accent-sqgreen-900 " 
+                checked={value === "conectados"}
+                onChange={handleOptionChange}
+            />
+            <label for="horizontal-list-radio-id" className="w-full py-3 px-2 ms-2 text-sm font-medium text-gray-900 ">Conectados</label>
+        </div>
+    </li>
+    <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r ">
+        <div className="flex items-center px-3">
+            <input 
+                id="horizontal-list-radio-license" 
+                type="radio" 
+                value="desconectados" 
+                name="list-radio" 
+                className="w-4 h-4 text-sqgreen-900 bg-gray-100 border-gray-300 focus:ring-sqgreen-900 accent-sqgreen-900  " 
+                checked={value === "desconectados"}
+                onChange={handleOptionChange}
+            />
+            <label for="horizontal-list-radio-military" className="w-full py-3 px-2 ms-2 text-sm font-medium text-gray-900 ">Desconectados</label>
+        </div>
+    </li>
+</ul>
+
+    </>
+  )
+}
--- src/pages/Dashboard/Pos.jsx
+++ src/pages/Dashboard/Pos.jsx
@@ -11,38 +11,63 @@ import BardCode from '../../assets/images/barCode.png';
 import { LinkCard } from '../../components/LinkCard'
 import { MapaTienda } from '../../components/MapaTienda'
 import { Input } from '../../components/Input'
+import { RadioSearchPos } from '../../components/RadioSearchPos'
 
 export const Pos = () => {
 
-  const { tiendas} = useSelector((state) => state.dataSlice);
-  
+  const { tiendas } = useSelector((state) => state.dataSlice);
+
   const [textTienda, setTextTienda] = useState('');
   const [tempTiendas, setTempTiendas] = useState([]);
-
+  const [selectRadio, setSelectRadio] = useState('todos')
 
   const filter = (text) => {
+    console.log(text, 'entra y por qué?')
     setTextTienda(text);
-    if(text.length === 0){
+    if (text.length === 0) {
       setTempTiendas(tiendas);
       return;
     };
 
 
     const newData = tiendas.filter(element => (element.id === (+text))
-    || element.descripcion.toLowerCase().includes(text.toLowerCase())
-    || element.ciudad.toLowerCase().includes(text)
-    || element.connected
-    
+      || element.descripcion.toLowerCase().includes(text.toLowerCase())
+      || element.ciudad.toLowerCase().includes(text)
     );
-
-    //element.id
-    //element.ciudad
-    //element.descripcion
     setTempTiendas(newData)
   }
 
+  const filterRadio = () => {
+    switch (selectRadio) {
+      case 'todos':
+          setTempTiendas(tiendas);
+        return;
+      case 'conectados':
+        const newData =  tiendas.filter(element=> element.connected)
+        setTempTiendas(newData);
+      return;
+      case 'desconectados':
+        const data =  tiendas.filter(element=> !element.connected)
+        setTempTiendas(data);
+      return;
+      default:
+        break;
+    }
+    const newData =  tiendas.filter()
+  }
+
+
+
+useEffect(() => {
+  filterRadio();
+  
+
+}, [selectRadio])
+
+
+
   useEffect(() => {
-    if(textTienda.length>0){
+    if (textTienda.length > 0) {
       filter(textTienda)
       return;
     }
@@ -64,20 +89,20 @@ export const Pos = () => {
         >
 
           <div className='flex items-center'>
-              <div className='w-6/12'>
-                <img src={BardCode} />
-              </div>
-              <div className={`w-6/12 tex-xs mulishBold  ${e.connected ? 'text-sqgreen-900' : 'text-red-600'}`}>
-                <p className='text-small'>ID {e.id} </p>
-                <p className='text-small'>{title[1]} </p>
-                <p className='text-small'>{e.ciudad} </p>
-              </div>
+            <div className='w-6/12'>
+              <img src={BardCode} />
+            </div>
+            <div className={`w-6/12 tex-xs mulishBold  ${e.connected ? 'text-sqgreen-900' : 'text-red-600'}`}>
+              <p className='text-small'>ID {e.id} </p>
+              <p className='text-small'>{title[1]} </p>
+              <p className='text-small'>{e.ciudad} </p>
+            </div>
           </div>
           <div className='border border-b-1 border-gray-400 my-4' />
           <div className={`text-white text-center rounded-sm  ${e.connected ? 'bg-sqgreen-900' : 'bg-red-600'}`}>{e.connected ? 'Activo' : 'Inactivo'}</div>
           <p className={`text-center mt-2 text-sqgreen-900 underline  ${e.connected ? 'text-sqgreen-900' : 'text-red-600'}`}>
             <FontAwesomeIcon icon={faShop} />
-           {" "} Ver tienda
+            {" "} Ver tienda
           </p>
         </LinkCard>
       )
@@ -92,14 +117,22 @@ export const Pos = () => {
           title={"Tiendas"}
           className='mt-24 justify-center mx-auto lg:w-11/12 '
         >
-          <p className='text-gray-700 mulishRegular'>Puedes filtrar por localidad o buscar por número de tienda.</p>
-          <div className='relative'>
-            <FontAwesomeIcon icon={faSearch} className='text-sqgreen-900 absolute left-2 top-8' />
-            <Input 
-              placeholder={"Buscar tienda"}
-              className={"w-72 md:w-72 rounded-md mt-6 bg-white h-8"}
-              onChange={filter}
-            />
+          <div className='w-full flex justify-between'>
+            <div className='relative w-6/12'>
+              <p className='text-gray-700 mulishRegular'>Puedes filtrar por localidad o buscar por número de tienda.</p>
+              <FontAwesomeIcon icon={faSearch} className='text-sqgreen-900 absolute left-2 top-14' />
+              <Input
+                placeholder={"Buscar tienda"}
+                className={"w-72 md:w-72 rounded-md mt-6 bg-white h-8"}
+                onChange={filter}
+              />
+            </div>
+            <div className='w-6/12 flex justify-end items-center'>
+              <RadioSearchPos
+                selectRadio={setSelectRadio}
+                value={selectRadio}
+              />
+            </div>
           </div>
           <div className='flex flex-wrap mt-8 justify-center h-3/6 overflow-auto pb-5'>
             {renderTiendas()}
