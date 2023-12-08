ac65ccca66f0a369ca5f1a2a6ea29fd0ffa36447
Dann Hernandez <dhernandez@superq.com.mx>
2023-12-07 18:44:10 -0600

    counting information is passed to the server

--- src/store/data/thunks.js
+++ src/store/data/thunks.js
@@ -6,34 +6,15 @@ import {
   setTiendas,
   startLoader,
   stopLoader,
-  setCountDisconnect
+  setCountDisconnect,
 } from "./dataSlice";
-
-
 
 export const getAllPOS = () => {
   return async (dispatch, getState) => {
     dispatch(startLoader());
     try {
       const { data } = await SDKZeus.getAllPOS();
-      const dataCount = await SDKZeus.getCountYesterday(0);
-      const tempData = data.data.datas.map((e) => {
-        const item = dataCount.data.data.find((e2) => e2.TIENDA === e.clave);
-        let valid = false
-        for (const key in item) {
-          if (key !== 'TIENDA') {
-            if (item[key] > 0) valid = true;
-          }
-        }
-        return {
-          ...e,
-          countYesterday: item,
-          warning : valid ? true: false
-        };
-      });
-
-    //   dispatch(setTiendas({ tiendas: data.data.datas }));
-      dispatch(setTiendas({ tiendas: tempData }));
+      dispatch(setTiendas({ tiendas: data.data }));
       if (data.data.datas.length > 0) {
         dispatch(setFlagTiendas({ flagTiendas: true }));
       }
@@ -75,9 +56,9 @@ export const getCentralLogConnection = () => {
   return async (dispatch, getState) => {
     try {
       const { data } = await SDKZeus.getLogConnection();
-      console.log(data.data.length, '75')
+      console.log(data.data.length, "75");
       await dispatch(setLogConnection(data.data));
-      dispatch(setCountDisconnect({countDisconnected: data.data.length}));
+      dispatch(setCountDisconnect({ countDisconnected: data.data.length }));
     } catch (err) {
       console.log(err);
     }
