cadce7f36692c26d0db14342c305d5101eed7779
Dann Hernandez <dhernandez@superq.com.mx>
2024-01-16 14:31:08 -0600

    Fix date log

--- src/utils/formatDates.js
+++ src/utils/formatDates.js
@@ -4,13 +4,18 @@
 export function convertIsoToDateTime(isoString) {
     const date = new Date(isoString);
 
-    const day = date.getDate().toString().padStart(2, '0');
+    const day = date.getUTCDate().toString().padStart(2, "0");
     const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Los meses comienzan en 0
     const year = date.getFullYear().toString().substr(-2); // Últimos dos dígitos del año
 
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    const seconds = date.getSeconds().toString().padStart(2, '0');
+    //const hours = date.getUTCHours().toString().padStart(2, '0');
+    let hours = date.getUTCHours();
+    const amPm = hours >= 12 ? 'PM' : 'AM';
+  
+    hours = hours % 12 || 12; // Convertir las horas al formato de 12 horas
+    const hoursStr = hours.toString().padStart(2, "0");
 
-    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+    const seconds = date.getUTCSeconds().toString().padStart(2, '0');    
+    return `${day}/${month}/${year} ${hoursStr}:${minutes}:${seconds} ${amPm}`;
 }
