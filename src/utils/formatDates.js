0a2a99f49976b4b6dd05a3272893866d9196cff0
Dann Hernandez <dhernandez@superq.com.mx>
2024-10-28 13:17:03 -0600

    Fix date on disconnections

--- src/utils/formatDates.js
+++ src/utils/formatDates.js
@@ -4,18 +4,14 @@
 export function convertIsoToDateTime(isoString) {
     const date = new Date(isoString);
 
-    const day = date.getUTCDate().toString().padStart(2, "0");
-    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Los meses comienzan en 0
-    const year = date.getFullYear().toString().substr(-2); // Últimos dos dígitos del año
-
-    //const hours = date.getUTCHours().toString().padStart(2, '0');
-    let hours = date.getUTCHours();
-    const amPm = hours >= 12 ? 'PM' : 'AM';
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = String(date.getFullYear()).slice(-2);
+    const hours = date.getHours() % 12 || 12;
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    const ampm = date.getHours() >= 12 ? 'PM' : 'AM';
   
-    hours = hours % 12 || 12; // Convertir las horas al formato de 12 horas
-    const hoursStr = hours.toString().padStart(2, "0");
-
-    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
-    const seconds = date.getUTCSeconds().toString().padStart(2, '0');    
-    return `${day}/${month}/${year} ${hoursStr}:${minutes}:${seconds} ${amPm}`;
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds} ${ampm}`;
+  
 }
